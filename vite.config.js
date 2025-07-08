2fadb5d9467cc02505a8968106b729fa54e68829
keppler_dev <sergio@tecknum.com>
2025-07-08 12:58:02 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// import dns from 'node:dns'
+import react from '@vitejs/plugin-react' // ✔️ usa Babel, NO swc
 
-// dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
